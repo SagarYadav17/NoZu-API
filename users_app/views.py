8251788a2b67164da394be72d5f60703d82f2f3a
Sagar Yadav <47110215+SagarYadav17@users.noreply.github.com>
2021-08-11 23:15:02 +0530

    Swagger added

--- users_app/views.py
+++ users_app/views.py
@@ -1,5 +1,7 @@
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.status import HTTP_200_OK, HTTP_406_NOT_ACCEPTABLE
 from users_app import serializers as users_serializers
 from users_app.models import Profile, Following
 from django.contrib.auth.models import User
@@ -11,8 +13,9 @@ class RegistrationValidatorView(ListAPIView):
     """
     View to validate already taken emails and usernames
     """
-    permission_classes = [AllowAny]
+    permission_classes = (AllowAny,)
     serializer_class = users_serializers.RegistrationValidationSerializer
+    queryset = User.objects.all()
 
 
 class RegisterView(CreateAPIView):
@@ -20,25 +23,47 @@ class RegisterView(CreateAPIView):
     View for New User Registration
     """
     queryset = User.objects.all()
-    permission_classes = [AllowAny]
+    permission_classes = (AllowAny,)
     serializer_class = users_serializers.RegisterSerializer
 
 
-class ProfileView(RetrieveUpdateDestroyAPIView):
+class ProfileView(RetrieveAPIView):
     """
     View for User Profile to Delete and Update
     """
-    permission_classes = [IsAuthenticated]
+    # permission_classes = (IsAuthenticated,)
     serializer_class = users_serializers.ProfileSerializer
     queryset = Profile.objects.all()
 
     def get_object(self):
+        username = self.kwargs['username']
         queryset = self.filter_queryset(self.get_queryset())
-        obj = queryset.get(user=self.request.user.id)
+        user_obj = get_object_or_404(User, username=username)
+        obj = queryset.get(user=user_obj)
+
         return obj
+
+    def post(self, request, username):
+        if username == self.request.user.username:
+            return Response(status=HTTP_200_OK)
+
+        else:
+            return Response({"error": "You don't have permission to perform that activity"}, status=HTTP_406_NOT_ACCEPTABLE)
+
+    def delete(self, request, username):
+        if username == self.request.user.username:
+            Profile.objects.filter(user__username=username).delete()
+            return Response({"status": "User Profile is deleted"}, status=HTTP_200_OK)
+
+        else:
+            return Response({"error": "You don't have permission to perform that activity"}, status=HTTP_406_NOT_ACCEPTABLE)
 
 
 class FollowingsView(ListAPIView):
+    """
+    List of profiles you follow
+    """
+    permission_classes = (IsAuthenticated,)
     serializer_class = users_serializers.FollowingSerializer
     queryset = Following.objects.all()
 
